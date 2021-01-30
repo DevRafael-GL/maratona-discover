14907c62bd4d7ce0c1ffd5262b833263124d3cdf
Rafael Gonçalves <rafaelgonsalveslima21@gmail.com>
2021-01-30 11:20:15 -0300

    Modal

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-
 const Modal = {
-  open () {
-    // Abrir modal
+  openAndClose () {
+    // Abrir & fechar modal
     // Adicionar a class active ao modal
-    modalOverlay.classList.add('active')
-  },
-  close(){
-    //fechar o modal
-    modalOverlay.classList.remove('active')
-    //remover a class active do modal
+   document.querySelector('.modal-overlay').classList.toggle('active')
   }
 }
